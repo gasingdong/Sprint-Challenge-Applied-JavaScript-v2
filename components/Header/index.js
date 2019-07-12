1f15add8d2a7eee85f58ab1a28ba7f752f29d7fa
TheIllusiveC4 <gasingdong@gmail.com>
2019-07-12 08:47:17 -0700

    complete header

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,12 +1,32 @@
-// STEP 1: Create a header component.
-// -----------------------
-// Using a function create the component you see below:
-//
-//  <div class="header">
-//    <span class="date">SMARCH 28, 2019</span>
-//    <h1>Lambda Times</h1>
-//    <span class="temp">98°</span>
-//  </div >
-// And add it to the DOM in the .headerContainer component
-
-function Header() {}
+function addClass(element, clazz) {
+  element.classList.add(clazz);
+}
+function addChild(parent, child) {
+  parent.appendChild(child);
+}
+function addChildren(parent, ...children) {
+  children.forEach(child => addChild(parent, child));
+}
+function createElement(type, clazz, textContent) {
+  const element = document.createElement(type);
+  if (clazz.length > 0) {
+    addClass(element, clazz);
+  }
+  if (textContent) {
+    element.textContent = textContent;
+  }
+  return element;
+}
+function createHeader() {
+  const header = createElement('div', 'header');
+  const date = createElement('span', 'date', 'SMARCH 28, 2019');
+  const headline = createElement('h1', '', 'Lambda Times');
+  const temp = createElement('span', 'temp', '98°');
+  addChildren(header, date, headline, temp);
+  return header;
+}
+const headerContainer = document.querySelector('.header-container');
+if (headerContainer) {
+  addChild(headerContainer, createHeader());
+}
+export { addClass, addChild, addChildren, createElement };
