bc9cf0ba91ebb0950a0ad2f343be44c30c156b57
TheIllusiveC4 <gasingdong@gmail.com>
2019-07-12 09:02:44 -0700

    format tabs index.js

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -1,15 +1,15 @@
 import { createElement, addChild } from '../Header/index.js';
 function createTab(topic) {
-    return createElement('div', 'tab', topic);
+  return createElement('div', 'tab', topic);
 }
 axios
-    .get('https://lambda-times-backend.herokuapp.com/topics')
-    .then((response) => {
+  .get('https://lambda-times-backend.herokuapp.com/topics')
+  .then(response => {
     const topics = document.querySelector('.topics');
     if (topics) {
-        response.data.topics.forEach((topic) => {
-            addChild(topics, createTab(topic));
-        });
+      response.data.topics.forEach(topic => {
+        addChild(topics, createTab(topic));
+      });
     }
-})
-    .catch((error) => console.log(error));
+  })
+  .catch(error => console.log(error));
