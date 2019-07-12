a40726e7f112f981c077e161cc56c1b0d2d04cc8
TheIllusiveC4 <gasingdong@gmail.com>
2019-07-12 09:33:21 -0700

    formatting

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -1,26 +1,26 @@
 import { createElement, addChild, addChildren } from '../Header/index.js';
 function createCard(data) {
-    const card = createElement('div', 'card');
-    const headline = createElement('div', 'headline', data.headline);
-    const author = createElement('div', 'author');
-    const img = createElement('img', '');
-    const byline = createElement('span', '', data.authorName);
-    img.src = data.authorPhoto;
-    addChildren(card, headline, author);
-    addChildren(author, img, byline);
-    return card;
+  const card = createElement('div', 'card');
+  const headline = createElement('div', 'headline', data.headline);
+  const author = createElement('div', 'author');
+  const img = createElement('img', '');
+  const byline = createElement('span', '', data.authorName);
+  img.src = data.authorPhoto;
+  addChildren(card, headline, author);
+  addChildren(author, img, byline);
+  return card;
 }
 axios
-    .get('https://lambda-times-backend.herokuapp.com/articles')
-    .then((response) => {
+  .get('https://lambda-times-backend.herokuapp.com/articles')
+  .then(response => {
     const cardsContainer = document.querySelector('.cards-container');
-    console.log(response.data.articles);
+    console.log(response);
     if (cardsContainer) {
-        const articles = response.data.articles;
-        for (const topic in response.data.articles) {
-            articles[topic].forEach((article) => {
-                addChild(cardsContainer, createCard(article));
-            });
-        }
+      const articles = response.data.articles;
+      for (const topic in response.data.articles) {
+        articles[topic].forEach(article => {
+          addChild(cardsContainer, createCard(article));
+        });
+      }
     }
-});
+  });
